cefc8e5736df3fcd29d585a0f273c8de4cfed1a3
Danny Adams <dantheado@gmail.com>
2020-10-08 12:18:47 +0100

    Configured gatsby-broweser and ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,26 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from "react"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import Theme from "./src/themes/theme"
 
-// You can delete this file if you're not using it
+const GloablStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+        background-color: pink;
+    }
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.theme.colors.light1};
+    
+    }
+`
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={Theme}>
+    <GloablStyles />
+    {element}
+  </ThemeProvider>
+)
