336fc93d9b0a3ae260554d5ae97f6d22e7b72707
Danny Adams <dantheado@gmail.com>
2020-10-08 11:20:33 +0100

    Initial commit and made a theme

---
+++ src/themes/theme.js
@@ -0,0 +1,32 @@
+export default {
+  fonts: {
+    main: "Helvetica, sans-serif",
+  },
+  colors: {
+    main1: "#232D9E",
+    main2: "#5361FF",
+    action: "#EBC53B",
+    paragraph: "#F4FBFF",
+  },
+  breakpoints: {
+    mobile: "only screen and (max-width: 40rem)",
+    tablet: "only screen and (max-width: 65rem)",
+  },
+  spacings: {
+    xxSmall: ".25rem",
+    xSmall: ".5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xLarge: "4rem",
+    xxLarge: "6rem",
+  },
+  animations: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "5px 10px 10px #888888",
+    shadow2: "3px 6px 12px #8A2BE2",
+  },
+}
