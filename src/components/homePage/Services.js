a3b52e014cdbd5cd5519e7d67df1fda1aa9331b8
Danny Adams <dantheado@gmail.com>
2020-10-09 20:23:36 +0100

    Made services section responsive

--- src/components/homePage/Services.js
+++ src/components/homePage/Services.js
@@ -5,25 +5,25 @@ import Img from 'gatsby-image'
 import { ServicesWrapper, ServicesModule, ServiceModulesContainer } from '../../elements/homePageElements/ServicesElements'
 
 export default function Services() {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
      webDesignImg: file(relativePath: { eq: "christopher-gower-m_HRfLhgABo-unsplash.jpg" }) {
         childImageSharp {
-          fixed(width: 290, height: 200) {
+          fixed(width: 300, height: 210) {
             ...GatsbyImageSharpFixed
           }
         }
       }
      SEOImg: file(relativePath: { eq: "nathana-reboucas-c4aT8MfEzdw-unsplash.jpg" }) {
         childImageSharp {
-          fixed(width: 290, height: 200) {
+          fixed(width: 300, height: 210) {
             ...GatsbyImageSharpFixed
           }
         }
       }
      googleAdsImg: file(relativePath: { eq: "matt-walsh-bPNGcFbEQMI-unsplash.jpg" }) {
         childImageSharp {
-          fixed(width: 290, height: 200) {
+          fixed(width: 300, height: 210) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -31,28 +31,28 @@ export default function Services() {
     }  
     `)
 
-    console.log(data);
+  console.log(data);
 
-    return (
-        <ServicesWrapper>
-            <h2>How I can help you get more customers</h2>
-            <ServiceModulesContainer>
-                <ServicesModule>
-                    <Img fixed={data.webDesignImg.childImageSharp.fixed} />
-                    <h3>Web Design</h3>
-                    <p>Sleek and modern web sites that make you look professional</p>
-                </ServicesModule>
-                <ServicesModule>
-                    <Img fixed={data.SEOImg.childImageSharp.fixed} />
-                    <h3>SEO</h3>
-                    <p>Search Engine Optimisation so you rank high on Google</p>
-                </ServicesModule>
-                <ServicesModule>
-                    <Img fixed={data.googleAdsImg.childImageSharp.fixed} />
-                    <h3>Google Ads</h3>
-                    <p>Advertise your products and services for targeted Google searches</p>
-                </ServicesModule>
-            </ServiceModulesContainer>
-        </ServicesWrapper>
-    )
+  return (
+    <ServicesWrapper>
+      <h2>How I can help you get more customers</h2>
+      <ServiceModulesContainer>
+        <ServicesModule>
+          <Img fixed={data.webDesignImg.childImageSharp.fixed} />
+          <h3>Web Design</h3>
+          <p>Sleek and modern web sites that make you look professional</p>
+        </ServicesModule>
+        <ServicesModule>
+          <Img fixed={data.SEOImg.childImageSharp.fixed} />
+          <h3>SEO</h3>
+          <p>Search Engine Optimisation so you rank high on Google</p>
+        </ServicesModule>
+        <ServicesModule>
+          <Img fixed={data.googleAdsImg.childImageSharp.fixed} />
+          <h3>Google Ads</h3>
+          <p>Advertise your products and services for targeted Google searches</p>
+        </ServicesModule>
+      </ServiceModulesContainer>
+    </ServicesWrapper>
+  )
 }
--- src/elements/homePageElements/ServicesElements.js
+++ src/elements/homePageElements/ServicesElements.js
@@ -4,8 +4,6 @@ export const ServicesWrapper = styled.section`
     background-color: ${props => props.theme.colors.main2};
     padding-top: 40px;
     padding-bottom: 40px;
-    /* padding-left: 125px;
-    padding-right: 125px; */
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -15,7 +13,9 @@ export const ServicesWrapper = styled.section`
         color: #EFF8FC;
         text-align: center;
         font-size: 1.8rem;
-        margin-bottom: 100px;
+        margin-bottom: 90px;
+        margin-right: 10px;
+        margin-left: 10px
     }
 `
 
@@ -26,6 +26,7 @@ export const ServiceModulesContainer = styled.div`
 
     @media only screen and (max-width: 60rem) {
         flex-direction: column;
+        align-items: center;
     }
 `
 
@@ -35,22 +36,21 @@ export const ServicesModule = styled.div`
    h3 {
        color: #D0EBF6;
        margin-top: 30px;
-       margin-bottom: 30px;
+       margin-bottom: 20px;
        font-size: 1.3rem;
    }
 
    p {
        color: #B3DEF1;
        line-height: 1.8rem;
-       font-size: 1.1rem;
+       font-size: 1.2rem;
        padding-right: 30px;
        padding-left: 30px;
    }
 
 
    @media only screen and (max-width: 60rem) {
-       display: flex;
-       border:3px solid black;
-       margin-left: 20px;
+      width: 300px;
+      margin-bottom: 50px;
     }
 `
